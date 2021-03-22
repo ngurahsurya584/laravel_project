adf89ad6c8b3c4ee9fae7894474972403c67de24
ngurahsurya584 <ngurahgusti584@gmail.com>
2021-03-22 23:44:45 +0800

    first

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Website Anom Jaya</title>
+</head>
+
+<body>
+
+    <section id="navbar" class="navbar">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <img src="asset/img/logo.jpg" width="7%" height="7%" class="d-inline-block align-top" alt="">
+                <a class="navbar-brand ml-3" href="#">Anom Jaya</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">About</a>
+                        <a class="nav-item nav-link" href="#">Katalog</a>
+                        <a class="nav-item nav-link disabled" href="#">Contact</a>
+                    </div>
+
+                    <div class="navbar-nav  ml-auto">
+                        <a class="nav-item nav-link" href="daftar.php">Daftar</a>
+                        <a class="nav-item nav-link" href="masuk.php">Masuk</a>
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+        </div>
+    </section>
+
+    <section id="jumbo" class="jumbo">
+        <div class="jumbotron jumbotron-fluid mt-5">
+            <div class="container text-center">
+                <img src="asset/img/banner.jpg" width="50%" height="50%" alt="">
+                <p class="lead-bold" style="background-color: white;">Kami siap melayani anda secara maksimal. All
+                    Available All Reliable :)</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="about" class="about">
+        <div class="container mb-5">
+            <div class="row">
+                <div class="col text-center mb-4">
+                    <h1>About</h1>
+                </div>
+            </div>
+
+            <div class="row text-justify">
+                <div class="col">
+                    <p>Anom Jaya merupakan Percetakan yang berlokasi di Kintamani, tepatnya di Jalan Raya Pasar
+                        Kintamani sebelah Bank BRI Unit Kintamani. Toko kami adalah percetakan terbaik yang bisa anda
+                        temukan di Kintamani</p>
+                </div>
+
+                <div class="col">
+                    <p>Toko kami melayani berbagai jenis percetakan dan menjual berbagai macam produk alat tulis sekolah
+                        dan kantor yang tentunya dengan harga yang terjangkau dan bersaing.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="katalog" class="katalog bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center mb-4 text-light">
+                    <h1>Katalog</h1>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-md-center mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="asset/img/buku.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Buku tulis merk Sinar Dunia berisi 38 lembar dengan kualitas yang
+                                sangat baik yang bisa membuat kita nyaman dalam menulis.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-center mb-2 ml-5">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="asset/img/pulpen.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Pulpen merk Queen dengan tinta yang baik dan harga yang murah tanpa
+                                mengurangi pengalaman kita menulis.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-centar mb-2 ml-5">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="asset/img/pensil.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Pensil merk Faber Castell yang sangat nyaman digunakan menulis dengan
+                                grafit kualitas tinggi tentu akan menghasilkan tulisan yang memuaskan.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <section id="contact" class="contact bg-light mb-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center mb-4">
+                        <h1>Contact</h1>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-md-center ml-2">
+                        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title-center-bold">FACEBOOK</h5>
+                                <p class="card-text">Anom Jaya</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-center ml-3">
+                        <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title ml-4">WHATSAPP</h5>
+                                <p class="card-text">(+62) 85-156-050-669</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-center ml-3">
+                        <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title-center-bold">INSTAGRAM</h5>
+                                <p class="card-text">@anom_jaya</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <section id="footer" class="footer">
+            <footer class="bg-dark text-white" height="40%">
+                <div class="container">
+                    <div class="row">
+                        <div class="col text-center">
+                            <p>Copyright*Ngurah_Surya_1915101040</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </section>
+
+
+
+</body>
+
+</html>
